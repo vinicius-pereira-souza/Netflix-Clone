a96e47036b95eaf70e8349671c8cdbe647801edf
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-07-24 16:44:21 -0300

    validar para aparecer a mensagem de erro completo

--- js/formulario.js
+++ js/formulario.js
@@ -1,58 +1,26 @@
 class Formulario {
   constructor() {
-    this.containerInputs = [...document.querySelectorAll('.inputContainer')]
-    this.btnMostrarSenha = document.querySelector('.mostrarSenha')
-    this.containerSenha = this.btnMostrarSenha.parentNode
-    this.btnSubmit = document.querySelector('.btnSubmit')
+    this.formulario = document.querySelector('form')
+    this.btnSubit = this.formulario.elements[3]
+    // this.containerInputs = document.querySelectorAll('.inputContainer')
+
     this.adicionarEventos()
   }
 
-  aniciarAnimacao = (e) => {
-    const container = e.target.parentNode
-    container.classList.add('focus')
-
-    if(e.target.parentNode.classList.contains('containerSenha')) {
-      this.btnMostrarSenha.classList.add('active')
-    }
-  }
-
-  validarInput = (e) => {
-    const input = e.target.parentNode.querySelector("input")
-    const msgErr = e.target.parentNode.nextElementSibling
-
-    if(e.type == 'keyup' && input.value == '') {
+  validarSubmit = (e) => {
+    e.preventDefault()
+    const input = e.target.parentNode.querySelector('input')
+    const inputContainer = e.target.parentNode
+    const msgErr = input.parentNode.nextElementSibling
+    
+    if(input.value == '') {
+      inputContainer.classList.add('errActive')
       msgErr.classList.add('activeErr')
     }
-
-  }
-
-  showPassWorld = (e) => {
-    e.preventDefault()
-    const inputSenha = this.containerSenha.querySelector('input')
-    if(inputSenha.getAttribute('type') == 'password') {
-      inputSenha.setAttribute('type', 'text')
-      e.target.innerText = 'ocultar'
-    } else {
-      inputSenha.setAttribute('type', 'password')
-      e.target.innerText = 'mostrar'
-    }
-  }
-
-  enviarFormulario = (e) => {
-    e.preventDefault()
   }
 
   adicionarEventos = () => {
-    this.containerInputs.forEach(container => {
-      const inputs = container.querySelector('input')
-
-      container.addEventListener('click', this.aniciarAnimacao)
-      inputs.addEventListener('keyup', this.validarInput)
-      inputs.addEventListener('change', this.validarInput)
-    })
-
-    this.btnMostrarSenha.addEventListener('click', this.showPassWorld)
-    this.btnSubmit.addEventListener('click', this.enviarFormulario)
+    this.formulario.addEventListener('change', this.validarSubmit)
   }
 }
 
