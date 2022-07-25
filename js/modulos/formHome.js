4252faefb03b5d57937de0883794329a59fc5fc0
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-07-25 16:59:00 -0300

    formulario de email da pagina index com comentarios completo

--- js/modulos/formHome.js
+++ js/modulos/formHome.js
@@ -1,69 +1,88 @@
 class FormHome {
   constructor(form, nome) {
     this.formulario = document.querySelector(`${form}[name="${nome}"]`)
-    this.containerDoFormulario = this.formulario.parentNode
+    this.containerInput = this.formulario.querySelector('.paddingPlaceholder')
     this.input = this.formulario.querySelector('input')
     this.btnSubmit = this.formulario.querySelector('button')
+    this.msg = this.formulario.querySelector('label')
+
     this.adicionarEventos()
   }
 
-  aniciarAnimacao = (e) => {
-    const placeholder = e.target.parentNode
-    placeholder.classList.add('active')
-    if(this.input.classList.contains('err')) {
-      this.input.classList.remove('err')
-    }
+  // animando o placeholder com o click
+  iniciarAnimacao = (e) => {
+    const container = e.target.parentNode
+
+    container.classList.add('active')
   }
 
-  validarValorInput = (e) => {
-    const msg = this.formulario.querySelector('label')
-    const terminarCom = this.input.value.endsWith('@gmail.com')
-    const espacoEmBranco = /\s/g.test(this.input.value)
+  // encerrando a animação do placeholder com focus e
+  // validando se o valor do input NÃO terminar com @gmail.com
+  encerraAnimacao = (e) => {
+    if(e.target.value == '') { 
+      e.target.parentNode.classList.remove('active')
 
-    if(e.type == 'change' && !terminarCom || espacoEmBranco) {
-      this.input.classList.add('err')
-      msg.innerText = 'Insira um email válido.'
-      msg.classList.add('err')
-
-    } else if (e.type == 'keyup' && this.input.value == '') {
-      this.input.classList.add('err')
-      msg.innerText = 'O email é obrigatório.'
-      msg.classList.add('err')
+    } else if(!this.input.value.endsWith('@gmail.com')) {
+      this.containerInput.classList.add('err')
+      this.msg.innerText = 'Insira um email válido.'
+      this.msg.classList.add('err')
     } 
   }
 
-  encerrarAnimacao = (e) => {
-    const containerDoInput = this.input.parentNode
-    const msg = this.formulario.querySelector('label')
-
-    if(e.target !== this.input && this.input.value == '') {
-      containerDoInput.classList.remove('active')
-      msg.classList.remove('err')
-    } 
-  }
-
-  enviarValor = (e) => {
+  // o primeiro if verifica se o input for vazio ao clicar para entrar, 
+  // se estiver vazio insere uma mensagem de erro e ativa o input com .focus()
+  // o segundo if verifica se o valor do input for valido 
+  // se for valido remove a mesagem de erro e limpa o input
+  validarEnviarDados = (e) => {
     e.preventDefault()
-    const msg = this.formulario.querySelector('label')
-    if(this.input.value === '') {
-      this.input.classList.add('err')
-      msg.innerText = 'O email é obrigatório.'
-      msg.classList.add('err')
-    } else if(this.input.value !== '' && this.input.value.endsWith('@gmail.com')) {
+
+    if(this.input.value == '') {
+      this.containerInput.classList.add('active')
+      this.containerInput.classList.add('err')
+      this.msg.innerText = 'O email é obrigatório.'
+      this.msg.classList.add('err')
+      this.input.focus()
+    }
+
+    if(this.input.checkValidity()) {
+      this.containerInput.classList.remove('err')
+      this.containerInput.classList.remove('active')
+      this.msg.classList.remove('err')
       this.input.value = ''
-      msg.classList.remove('err')
-      this.input.classList.remove('err')
     }
   }
 
+  // o primeiro if verifica se a classe de err esta sendo usada no container do input
+  // o segundo if faz a mesma coisa que o primeiro só que também verifica se o input esta vazio
+  // o terceiro if faz a mesma coisa que os anteriores mas também verifica a classe err
+  // esta sendo usada e se o valor do input termina com @gmail.com, se terminar remove as classes
+  validarValorInput = (e) => {
+    const container = this.containerInput
+    const terminarCom = this.input.value.endsWith('@gmail.com')
+
+    if(container.classList.contains('err')) {
+      this.msg.innerText = 'Insira um email válido.'
+    } 
+    
+    if(container.classList.contains('err') && this.input.value == '') {
+      this.msg.innerText = 'O email é obrigatório.'
+    }
+
+    if(container.classList.contains('err') && terminarCom) {
+      container.classList.remove('err')
+      this.msg.classList.remove('err')
+    }
+  }
+
+  // adiciona os eventos aos elementos
   adicionarEventos = () => {
-    this.input.addEventListener('click', this.aniciarAnimacao)
-    this.input.addEventListener('change', this.validarValorInput)
+    this.containerInput.addEventListener('click', this.iniciarAnimacao)
+    this.input.addEventListener('focusout', this.encerraAnimacao)
     this.input.addEventListener('keyup', this.validarValorInput)
-    this.containerDoFormulario.addEventListener('click', this.encerrarAnimacao)
-    this.btnSubmit.addEventListener('click', this.enviarValor)
+    this.btnSubmit.addEventListener('click', this.validarEnviarDados)
   }
 
 }
 
 export default FormHome
+// const formHome1 = new FormHome('form', 'formHeader')
